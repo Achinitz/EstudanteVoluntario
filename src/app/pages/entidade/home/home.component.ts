23233cbf11b1ad763eb8e63d487f89d3d03f588a
Gustavo Achinitz <gustavo.oliveira@fcopel.org.br>
2022-07-24 15:18:03 -0300

    Alteraçoes realizadas pela Amanda

--- src/app/pages/entidade/home/home.component.ts
+++ src/app/pages/entidade/home/home.component.ts
@@ -9,6 +9,59 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  
+
+  vagasDisponiveis: any = [
+    {
+      id: 1,
+      nomeEntidade: 'APAE 1',
+      nomeVaga: 'Contador de História',      
+      img: '../../assets/imagens/mulherGrandeCoracao.jpg',
+      descricao: 'Vaga para pessoas de bom coração',
+      status: 'Aberto'
+    },
+    {
+      id: 2,
+      nomeEntidade: 'APAE 2',
+      nomeVaga: 'Contador de História',
+      img: '../../assets/imagens/mulherGrandeCoracao.jpg',
+      descricao: 'Vaga para pessoas de bom coração',
+      status: 'Aberto'
+    },
+    {
+      id: 3,
+      nomeEntidade: 'APAE 3',
+      nomeVaga: 'Contador de História',      
+      img: '../../assets/imagens/mulherGrandeCoracao.jpg',
+      descricao: 'Vaga para pessoas de bom coração',
+      status: 'Aberto'
+    },
+    {
+      id: 4,
+      nomeEntidade: 'APAE 4',
+      nomeVaga: 'Contador de História',      
+      img: '../../assets/imagens/mulherGrandeCoracao.jpg',
+      descricao: 'Vaga para pessoas de bom coração',
+      status: 'Aberto'
+    },
+    {
+      id: 5,
+      nomeEntidade: 'APAE 5',
+      nomeVaga: 'Contador de História',
+      img: '../../assets/imagens/mulherGrandeCoracao.jpg',
+      descricao: 'Vaga para pessoas de bom coração',
+      status: 'Aberto'
+    },
+    {
+      id: 6,
+      nomeEntidade: 'APAE 6',
+      nomeVaga: 'Contador de História',      
+      img: '../../assets/imagens/mulherGrandeCoracao.jpg',
+      descricao: 'Vaga para pessoas de bom coração',
+      status: 'Aberto'
+    },
+  ];
+
   ngOnInit(): void {
   }
 
