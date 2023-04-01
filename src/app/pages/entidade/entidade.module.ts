2b3230ff14c2a70770d82fa47e3cc21ff6288f7d
akmazurok <ak.mazurok@gmail.com>
2023-04-01 00:07:28 -0300

    Perfil Entidade e Estudante

--- src/app/pages/entidade/entidade.module.ts
+++ src/app/pages/entidade/entidade.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { EntidadeRoutingModule } from './entidade-routing.module';
+
 import { HomeComponent } from './home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { PerfilComponent } from './perfil/perfil.component';
@@ -15,9 +16,14 @@ import { AvaliarEstudanteComponent } from './avaliar-estudante/avaliar-estudante
 import { ModalVisualizarComponent } from './avaliar-estudante/modal-visualizar/modal-visualizar.component';
 import { ModalAdicionarAvaliacaoComponent } from './avaliar-estudante/modal-adicionar-avaliacao/modal-adicionar-avaliacao.component';
 import { EntidadeComponent } from './entidade.component';
+import { VagasComponent } from './vagas/vagas.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgxMaskModule } from 'ngx-mask';
+
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
@@ -26,7 +32,8 @@ import { MatListModule } from '@angular/material/list';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { VagasComponent } from './vagas/vagas.component';
+
+import { DataService } from 'src/app/services/data.service';
 
 @NgModule({
   declarations: [
@@ -49,11 +56,13 @@ import { VagasComponent } from './vagas/vagas.component';
     CommonModule,
     EntidadeRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    NgSelectModule,
+    NgxMaskModule,
     MatTabsModule,
     MatDatepickerModule,
     MatPaginatorModule,
-    NgbModule,
-    ReactiveFormsModule,
     MatSidenavModule,
     MatIconModule,
     MatDividerModule,
@@ -62,5 +71,6 @@ import { VagasComponent } from './vagas/vagas.component';
     MatToolbarModule,
     MatButtonModule,
   ],
+  providers: [DataService],
 })
 export class EntidadeModule {}
--- src/app/pages/entidade/perfil/perfil.component.ts
+++ src/app/pages/entidade/perfil/perfil.component.ts
@@ -1,19 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EnderecoService } from 'src/app/services/endereco.service';
+import { ConsultaCepService } from 'src/app/services/consulta-cep.service';
+import { genericAnimations } from 'src/app/shared/animations/animations';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.scss']
+  styleUrls: ['./perfil.component.scss'],
+  animations: genericAnimations,
 })
 export class PerfilComponent implements OnInit {
+  submitted = false;
+
+  cnpj = '00000000000000';
+  razaoSocial = 'Razão Social';
+  nomeFantasia = 'Nome Fantasia';
+
+  estado: any;
+  cidade: any;
+  bairro: any;
+  resultadoCep: any;
+  cidades: any = [];
+  bairros: any = [];
+  estados: any = [];
 
   public formCadastro = new FormGroup({
-    cnpj: new FormControl(null,[Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$')]),
-    razaoSocial: new FormControl(null, Validators.required),
-    nomeFantasia: new FormControl(null, Validators.required),
+    cnpj: new FormControl(
+      { value: this.cnpj, disabled: true },
+      Validators.required
+    ),
+    imgPerfil: new FormControl(null, Validators.required),
+    missao: new FormControl(null, Validators.required),
+    perfilVoluntario: new FormControl(null, Validators.required),
+    razaoSocial: new FormControl(
+      { value: this.razaoSocial, disabled: true },
+      Validators.required
+    ),
+    nomeFantasia: new FormControl(
+      { value: this.nomeFantasia, disabled: true },
+      Validators.required
+    ),
     nomeResponsavelCadastro: new FormControl(null, Validators.required),
-    email: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
     telefone: new FormControl(null, Validators.required),
     cep: new FormControl(null, Validators.required),
     logradouro: new FormControl(null, Validators.required),
@@ -23,12 +53,86 @@ export class PerfilComponent implements OnInit {
     estado: new FormControl(null, Validators.required),
     cidade: new FormControl(null, Validators.required),
     senha: new FormControl(null, Validators.required),
-    termo: new FormControl(null, Validators.required),
+    confirmarSenha: new FormControl(null, Validators.required),
   });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  mostrarValores() {
+    console.log('Formulário enviado');
   }
 
+  constructor(
+    private consultaCepService: ConsultaCepService,
+    private enderecoService: EnderecoService,
+    private toast: ToastrService
+  ) {
+    this.inicializaFormulario();
+  }
+
+  inicializaFormulario() {
+    this.enderecoService.getEstados().subscribe((data: any) => {
+      this.estados = data;
+    });
+  }
+
+  onAddCidade() {
+    this.enderecoService
+      .getCidades(this.formCadastro.get('estado')?.value)
+      .subscribe((data: any) => {
+        this.cidades = data;
+      });
+  }
+
+  onAddBairro() {
+    // console.log('Inicio cidade selecionado');
+    // console.log(this.formCadastro.get('cidade')?.value);
+    //console.log('Fim cidade selecionado');
+  }
+
+  validaCep() {
+    if (this.formCadastro.get('cep')?.value.length === 8) {
+      let cep = this.formCadastro.get('cep')?.value;
+      this.consultaCepService.getDataCep(cep.replace('-', '')).subscribe(
+        (data: any) => {
+          if (data.erro === true) {
+            this.toast.error('CEP Inválido!');
+          }
+          this.resultadoCep = data;
+          //  console.log(data);
+          this.estados.forEach((element: any) => {
+            if (element.sigla === data.uf) {
+              this.formCadastro.get('estado')?.setValue(element.id);
+            }
+          });
+
+          setTimeout(() => {
+            this.cidades.forEach((element: any) => {
+              if (element.nome === data.localidade) {
+                this.formCadastro.get('cidade')?.setValue(element.id);
+                this.formCadastro.get('logradouro')?.setValue(data.logradouro);
+                this.formCadastro.get('bairro')?.setValue(data.bairro);
+              }
+            });
+          }, 1500);
+        },
+        (error) => {
+          console.log('Ocorreu um erro');
+        }
+      );
+      console.log('CEP ok');
+    }
+  }
+
+  validarSenha() {
+    if (
+      this.formCadastro.get('novaSenha')?.value ==
+        this.formCadastro.get('confirmarSenha')?.value &&
+      !this.formCadastro.get('novaSenha')?.invalid &&
+      !this.formCadastro.get('confirmarSenha')?.invalid
+    ) {
+    } else {
+      this.toast.error('As senhas não são iguais!');
+    }
+  }
+
+  ngOnInit(): void {}
 }
