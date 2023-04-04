9430a37d755b9bc95316b34f2b740a053c74d915
akmazurok <ak.mazurok@gmail.com>
2023-04-04 20:02:20 -0300

    Add IES, Add Curso e Add Admin ok

--- src/app/pages/administrador/cadastrar-curso/adicionar-curso/adicionar-curso.component.ts
+++ src/app/pages/administrador/cadastrar-curso/adicionar-curso/adicionar-curso.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { ConsultaCepService } from "src/app/services/consulta-cep.service";
-import { EnderecoService } from "src/app/services/endereco.service";
-import { ToastrService } from "ngx-toastr";
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EnderecoService } from 'src/app/services/endereco.service';
+import { ConsultaCepService } from 'src/app/services/consulta-cep.service';
+import { genericAnimations } from 'src/app/shared/animations/animations';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: "app-adicionar-curso",
-  templateUrl: "./adicionar-curso.component.html",
-  styleUrls: ["./adicionar-curso.component.scss"],
+  selector: 'app-adicionar-curso',
+  templateUrl: './adicionar-curso.component.html',
+  styleUrls: ['./adicionar-curso.component.scss'],
+  animations: genericAnimations,
 })
 export class AdicionarCursoComponent implements OnInit {
   estado: any;
@@ -21,63 +23,33 @@ export class AdicionarCursoComponent implements OnInit {
   graus: any = [
     {
       id: 1,
-      nome: "Bacharelado",
+      nome: 'Bacharelado',
     },
     {
       id: 2,
-      nome: "Licenciatura",
+      nome: 'Licenciatura',
     },
     {
       id: 3,
-      nome: "Tecnologia",
+      nome: 'Tecnologia',
     },
   ];
 
   instituicoes: any = [
+    { id: 1, nome: 'UFPR - Universidade Federal do Paraná - 75054940000162' },
     {
-      cnpj: 75054940000162,
-      nome: 'UNIVERSIDADE FEDERAL DO PARANÁ',
-      sigla: 'UFPR'
+      id: 2,
+      nome: 'UTFPR - Universidade Tecnológica Federal do Paraná - 75054940000162',
     },
-    {
-      cnpj: 75054940000162,
-      nome: 'UNIVERSIDADE TECNOLÓGICA FEDERAL DO PARANÁ',
-      sigla: 'UTFPR'
-    },
-    {
-      cnpj: 75054940000162,
-      nome: 'PONTIFÍCIA UNIVERSIDADE CATÓLICA DO PARANÁ',
-      sigla: 'PUCPR'
-    },
-    {
-      cnpj: 75054940000162,
-      nome: 'UNIVERSIDADE POSITIVO',
-      sigla: 'UP'
-    },
-    {
-      cnpj: 75054940000162,
-      nome: 'CENTRO UNIVERSITÁRIO CURITIBA',
-      sigla: 'UNICURITIBA'
-    },
-    {
-      cnpj: 75054940000162,
-      nome: 'CENTRO UNIVERSITÁRIO INTERNACIONAL',
-      sigla: 'UNINTER'
-    },
-    {
-      cnpj: 75054940000162,
-      nome: 'CENTRO UNIVERSITÁRIO AUTÔNOMO DO BRASIL',
-      sigla: 'UNIBRASIL'
-    },    
   ];
-  
+
 
   formCadastro: FormGroup = new FormGroup({
     cnpj: new FormControl(null, [
       Validators.required,
-      Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$"),
+      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$'),
     ]),
-    ies: new FormControl(null, Validators.required),
+    instituicao: new FormControl(null, Validators.required),
     grau: new FormControl(null, Validators.required),
     nomeCurso: new FormControl(null, Validators.required),
     campus: new FormControl(null, Validators.required),
@@ -101,67 +73,56 @@ export class AdicionarCursoComponent implements OnInit {
   inicializaFormulario() {
     this.enderecoService.getEstados().subscribe((data: any) => {
       this.estados = data;
-      console.log("Inicio estados");
-      console.log(data);
-      console.log("Fim estados");
     });
   }
 
   onAddCidade() {
-    // if(this.formCadastro.get("estado")?.value === null && ){
-    //   this.formCadastro.get('cidade')?.setValue(null)
-    // }
     this.enderecoService
-      .getCidades(this.formCadastro.get("estado")?.value)
+      .getCidades(this.formCadastro.get('estado')?.value)
       .subscribe((data: any) => {
         this.cidades = data;
       });
   }
 
   onAddBairro() {
-    console.log("Inicio cidade selecionado");
-    console.log(this.formCadastro.get("cidade")?.value);
-    console.log("Fim cidade selecionado");
+    // console.log('Inicio cidade selecionado');
+    // console.log(this.formCadastro.get('cidade')?.value);
+    //console.log('Fim cidade selecionado');
   }
 
   validaCep() {
-    console.log(this.formCadastro.get("cep")?.value.length);
-    if (this.formCadastro.get("cep")?.value.length === 8) {
-      let cep = this.formCadastro.get("cep")?.value;
-      this.consultaCepService.getDataCep(cep.replace("-", "")).subscribe(
+    if (this.formCadastro.get('cep')?.value.length === 8) {
+      let cep = this.formCadastro.get('cep')?.value;
+      this.consultaCepService.getDataCep(cep.replace('-', '')).subscribe(
         (data: any) => {
           if (data.erro === true) {
-            this.toast.error("CEP Inválido!");
+            this.toast.error('CEP Inválido!');
           }
           this.resultadoCep = data;
-          console.log(data);
+          //  console.log(data);
           this.estados.forEach((element: any) => {
             if (element.sigla === data.uf) {
-              this.formCadastro.get("estado")?.setValue(element.id);
+              this.formCadastro.get('estado')?.setValue(element.id);
             }
           });
 
           setTimeout(() => {
             this.cidades.forEach((element: any) => {
               if (element.nome === data.localidade) {
-                this.formCadastro.get("cidade")?.setValue(element.id);
-                this.formCadastro.get("logradouro")?.setValue(data.logradouro);
-                this.formCadastro.get("bairro")?.setValue(data.bairro);
+                this.formCadastro.get('cidade')?.setValue(element.id);
+                this.formCadastro.get('logradouro')?.setValue(data.logradouro);
+                this.formCadastro.get('bairro')?.setValue(data.bairro);
               }
             });
           }, 1500);
         },
         (error) => {
-          console.log("Ocorreu um erro");
+          console.log('Ocorreu um erro');
         }
       );
-      console.log("saiu na api");
+      console.log('CEP ok');
     }
   }
-
-  mostrarValores() {
-    console.log("Formulário enviado");
-  }
-
+ 
   ngOnInit(): void {}
 }
