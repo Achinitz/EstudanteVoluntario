86a6af2b44c270c8675596884bb6cb44a84fd339
akmazurok <ak.mazurok@gmail.com>
2023-05-19 17:13:24 -0300

    Entidade - Inscrição na vaga - detalhe ok

--- src/app/pages/entidade/vagas/detalhes/detalhe-estudante/detalhe-estudante.component.ts
+++ src/app/pages/entidade/vagas/detalhes/detalhe-estudante/detalhe-estudante.component.ts
@@ -7,16 +7,15 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-detalhe-estudante',
   templateUrl: './detalhe-estudante.component.html',
-  styleUrls: ['./detalhe-estudante.component.scss']
+  styleUrls: ['./detalhe-estudante.component.scss'],
 })
 export class DetalheEstudanteComponent implements OnInit {
+  @Input() inscricao: any;
 
-  perfilCadidato: any = {
+  //buscar os dados do Estudante de acordo com o ID da inscrição
+  perfilCandidato: any = {
     nome: 'Gustavo de Oliveira Achinitz',
-    idade: '23',
     nomeSocial: 'Natasha de Oliveira',
-    curriculum: '',
-    foto: '',
     identificacaoGenero: 'Transexual',
     dataNascimento: '01/03/1997',
     email: 'gustavoachinitz@gmail.com',
@@ -25,29 +24,28 @@ export class DetalheEstudanteComponent implements OnInit {
     logradouro: 'casa',
     numero: '1133',
     bairro: 'bairro',
-    estado: 'Paraná',
+    estado: 'PR',
     cidade: 'Curitiba',
     complemento: 'Casa',
-    cursos: [
-      {nome: 'Torneiro Mecânico', instituicao: 'SENAI', dataInicio: '01/01/2015', dataPrevistoTermino: '12/12/2015', cargaHoraria: '350 Horas', descricao: 'Curso de torneiro mechendo com Torno, fresa e solda.', situacao: 'Concluido'},
-      {nome: 'Técnico em Fabricação Mecânica', instituicao: 'IFSC - Instituto Federal de Santa Catarina', dataInicio: '05/06/2013', dataPrevistoTermino: '12/12/2015', cargaHoraria: '1520 Horas', descricao: '', situacao: 'Concluido'},
-      {nome: 'Analise e Desenvolvimento de Sistemas', instituicao: 'UFPR - Universidade Federal do Paraná', dataInicio: '02/02/2018', dataPrevistoTermino: '15/07/2023', cargaHoraria: '', descricao: 'Curso bom', situacao: 'Concluido'},
-    ],
-    experienciasTrabalho: [
-      {cargo: 'Estagiário', empresa: 'Tribunal de Contas do Estado do Paraná', dataInicio: '01/03/2019', dataSaide: '12/12/2021', cargaHoraria: '350 Horas', atividades: ''},
-    ],
-    certificados: null,
-  }
+    curso: 'Tecnologia em Análise de Sistemas',
+    grau: 'Tecnologia',
+    instituicao: 'SENAI',
+    anoInicio: '2015',
+    anoConclusao: '2025',
+    experienciasAnteriores: null,
+    areasInteresse: null,
+    status: 'Inscrito'      
+  };
 
-  @Input() candidato: any;
+  constructor(
+    private data: DataService,
+    private router: Router,
+    public activeModal: NgbActiveModal
+  ) {}
 
-  constructor(private data:DataService, private router: Router, public activeModal: NgbActiveModal) { }
+  downloadCertificado(certificado: any) {}
 
-  downloadCertificado(certificado:any){
-
-  }
-
-  aprovarCandidato(){
+  aprovarCandidato() {
     Swal.fire({
       title: 'Deseja aprovar esse candidato para essa vaga?',
       icon: 'warning',
@@ -55,24 +53,22 @@ export class DetalheEstudanteComponent implements OnInit {
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Confirmar',
-      cancelButtonText: 'Cancelar'
+      cancelButtonText: 'Cancelar',
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire({
           title: 'Candidato aprovado com sucesso!',
           icon: 'success',
           showConfirmButton: false,
-          timer: 1500
-        }).then(
-          () => {
-            this.activeModal.close('Aprovado');
-          }
-        )
+          timer: 1500,
+        }).then(() => {
+          this.activeModal.close('Aprovado');
+        });
       }
-    })
+    });
   }
 
-  reprovarCandidato(){
+  reprovarCandidato() {
     Swal.fire({
       title: 'Deseja reprovar esse candidato para essa vaga?',
       icon: 'warning',
@@ -80,24 +76,20 @@ export class DetalheEstudanteComponent implements OnInit {
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Confirmar',
-      cancelButtonText: 'Cancelar'
+      cancelButtonText: 'Cancelar',
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire({
           title: 'Candidato Reprovado com sucesso!',
           icon: 'success',
           showConfirmButton: false,
-          timer: 1500
-        }).then(
-          () => {
-            this.activeModal.close('Reprovado');
-          }
-        )
+          timer: 1500,
+        }).then(() => {
+          this.activeModal.close('Reprovado');
+        });
       }
-    })
+    });
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/entidade/vagas/detalhes/detalhes.component.ts
+++ src/app/pages/entidade/vagas/detalhes/detalhes.component.ts
@@ -8,73 +8,33 @@ import { DetalheEstudanteComponent } from './detalhe-estudante/detalhe-estudante
 @Component({
   selector: 'app-detalhes',
   templateUrl: './detalhes.component.html',
-  styleUrls: ['./detalhes.component.scss']
+  styleUrls: ['./detalhes.component.scss'],
 })
 export class DetalhesComponent implements OnInit, OnDestroy {
-
   vaga: any;
-  modalReference: any;
-
-  requisitos: any = [
-    {descricao: 'Ter 5 pós graduação'},
-    {descricao: 'Algum ai'},
-    {descricao: 'Algum ai'},
-  ]
-
-  inscritos: any = [
-    {
-      nome: 'Jeronimo',
-      img: null,
-      cpf: '123',
-      curso: 'Analise de Sistemas',
-      idade: 22,
-      dataInscricao: '15/05/2022',
-      status: 'Andamento'
-    },
-    {
-      nome: 'Alienigena',
-      img: null,
-      cpf: '1234',
-      curso: 'Analise de Sistemas',
-      idade: 22,
-      dataInscricao: '15/05/2022',
-      status: 'Andamento'
-    },
-    {
-      nome: 'Alex',
-      img: null,
-      cpf: '12345',
-      curso: 'Analise de Sistemas',
-      idade: 22,
-      dataInscricao: '15/05/2022',
-      status: 'Andamento'
-    },
-    {
-      nome: 'Tebutrios',
-      img: null,
-      cpf: '123456',
-      curso: 'Analise de Sistemas',
-      idade: 22,
-      dataInscricao: '15/05/2022',
-      status: 'Andamento'
-    }
-  ]
-
-  constructor(private dataService: DataService,private router: Router, private modalService: NgbModal) { 
+ 
+  constructor(
+    private dataService: DataService,
+    private router: Router,
+    private modalService: NgbModal
+  ) {
     this.vaga = this.dataService.data;
-    if(this.dataService.data.nomeEntidade == null){
+    if (this.dataService.data.nomeEntidade == null) {
       this.router.navigate(['/Entidade/vagas']);
     }
   }
 
-  visualizarCandidato(candidato:any){
+  visualizarCandidato(inscricao: any) {
     // this.router.navigate(['/Entidade/detalhe-estudante']);
-    const modalRef = this.modalService.open(DetalheEstudanteComponent, { windowClass: 'min-width: 80%; heigth: 50%;', centered: true });
-    modalRef.componentInstance.candidato = candidato;
-    modalRef.result
+    const modalRef = this.modalService.open(DetalheEstudanteComponent, {
+      windowClass: 'min-width: 80%; heigth: 50%;',
+      centered: true,
+    });
+    modalRef.componentInstance.inscricao = inscricao; 
+    modalRef.result;
   }
 
-  removerCandidato(candidato:any){
+  removerCandidato(inscrito: any) {
     Swal.fire({
       title: 'Deseja reprovar esse candidato para essa vaga?',
       icon: 'warning',
@@ -82,20 +42,19 @@ export class DetalhesComponent implements OnInit, OnDestroy {
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Confirmar',
-      cancelButtonText: 'Cancelar'
+      cancelButtonText: 'Cancelar',
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire({
           title: 'Candidato Reprovado com sucesso!',
           icon: 'success',
           showConfirmButton: false,
-          timer: 1500
-        })
+          timer: 1500,
+        });
       }
-    })
+    });
   }
 
-  
   finalizarPorUrgencia() {
     Swal.fire({
       title: 'Deseja realmente finalizar esse processo seletivo?',
@@ -142,12 +101,9 @@ export class DetalhesComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.dataService.data = this.vaga;
-
   }
-
 }
