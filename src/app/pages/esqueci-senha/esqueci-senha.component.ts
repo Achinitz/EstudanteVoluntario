2633971a8a751a1782ecf298ccce1964b2e01dbd
akmazurok <ak.mazurok@gmail.com>
2023-05-24 18:58:55 -0300

    Recuperação senha

--- src/app/pages/esqueci-senha/esqueci-senha.component.ts
+++ src/app/pages/esqueci-senha/esqueci-senha.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-esqueci-senha',
   templateUrl: './esqueci-senha.component.html',
-  styleUrls: ['./esqueci-senha.component.scss']
+  styleUrls: ['./esqueci-senha.component.scss'],
 })
 export class EsqueciSenhaComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  solicitarRecuperacaoSenha(){
+  solicitarRecuperacaoSenha() {
+    Swal.fire({
+      title: `O link de recuperação foi enviado para o e-mail de cadastro.`,
+      icon: 'success',
+      showConfirmButton: false,
+      timer: 3000,
+    });
     this.router.navigate(['/login']);
   }
-
 }
