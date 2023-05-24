7a42ae778d6f4892f2e16c0f3be8d621a6f85bf0
akmazurok <ak.mazurok@gmail.com>
2023-05-24 18:10:03 -0300

    Página 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,43 +4,54 @@ import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { EsqueciSenhaComponent } from './pages/esqueci-senha/esqueci-senha.component';
+import { Pagina404Component } from './pages/pagina404/pagina404.component';
 
 const routes: Routes = [
   {
-    path: "", redirectTo: "/home", pathMatch: "full"
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
   },
-  { 
-    path:"home",component: HomeComponent 
+  {
+    path: 'home',
+    component: HomeComponent,
   },
-  { 
-    path:"cadastro",component: CadastroComponent 
+  {
+    path: 'cadastro',
+    component: CadastroComponent,
   },
-  { 
-    path:"login",component: LoginComponent 
+  {
+    path: 'login',
+    component: LoginComponent,
   },
-  { 
-    path:"esqueceu-senha",component: EsqueciSenhaComponent 
+  {
+    path: 'esqueceu-senha',
+    component: EsqueciSenhaComponent,
   },
-  { 
-    path:"Admin", loadChildren: () => import('./pages/administrador/administrador.module').then(
-      (m) => m.AdministradorModule
-    ) 
+  {
+    path: 'Admin',
+    loadChildren: () =>
+      import('./pages/administrador/administrador.module').then(
+        (m) => m.AdministradorModule
+      ),
   },
-  { 
-    path:"Estudante", loadChildren: () => import('./pages/estudante/estudante.module').then(
-      (m) => m.EstudanteModule
-    )
+  {
+    path: 'Estudante',
+    loadChildren: () =>
+      import('./pages/estudante/estudante.module').then(
+        (m) => m.EstudanteModule
+      ),
   },
-  { 
-    path:"Entidade", loadChildren: () => import('./pages/entidade/entidade.module').then(
-      (m) => m.EntidadeModule
-    )
+  {
+    path: 'Entidade',
+    loadChildren: () =>
+      import('./pages/entidade/entidade.module').then((m) => m.EntidadeModule),
   },
-
+  { path: '**', pathMatch: 'full', component: Pagina404Component },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
