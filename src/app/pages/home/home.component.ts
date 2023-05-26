2cef73c6107ee476112a9491ac5dbc21b4a72823
akmazurok <ak.mazurok@gmail.com>
2023-05-25 23:01:27 -0300

    Início das alterações da home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -74,36 +74,6 @@ export class HomeComponent implements OnInit {
       content:
         'Além de aprender, ajudar e viver incríveis experiências. Você pode usar as horas trabalhadas como horas formativas.',
     },
-    {
-      img: '../../assets/imagens/mulherGrandeCoracao.png',
-      title: 'Transformação',
-      content:
-        'Embora o objetivo do voluntário seja melhorar a vida de outras pessoas, ele acaba melhorando muito a sua própria existência também.',
-    },
-    {
-      img: '../../assets/imagens/pessoasDandoAsMaos.png',
-      title: 'Maior conexão social',
-      content:
-        'Voluntariar para adquiri ou gostar de contato social, sair de casa e explorar suas próprias forças.',
-    },
-    {
-      img: '../../assets/imagens/ideia.png',
-      title: 'Desenvolver ideias',
-      content:
-        'Aproveite a liberdade que o trabalho voluntário proporciona e desenvola ideias geniais.',
-    },
-    {
-      img: '../../assets/imagens/experienciasValiosass.png',
-      title: 'Experiências valiosas',
-      content:
-        'O contato com várias pessoas em diferentes condições de vida e saúde, transforma vidas.',
-    },
-    {
-      img: '../../assets/imagens/horasFormativas.png',
-      title: 'Horas formativas',
-      content:
-        'Além de aprender, ajudar e viver incríveis experiências. Você pode usar as horas trabalhadas como horas formativas.',
-    },
   ];
 
   lermais: boolean = true;
